e5b51523f5ec56fcdb754fa5ec0b5d73d44d1857
bersyatina <aw_apanasenko@mail.ru>
2023-10-06 11:28:51 +0500

    step 2

--- gendiff.js
+++ gendiff.js
@@ -5,7 +5,10 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information')
   .version('0.8.0');
+
+program.option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 
 program.parse();
