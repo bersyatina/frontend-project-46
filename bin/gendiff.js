206ac9e028a156e38adc3eba9f41fbfccf1fbea9
bersyatina <aw_apanasenko@mail.ru>
2023-11-24 11:04:02 +0500

    restart stage 3

---
+++ bin/gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+import genDiff from '../src/index.js';
+
+program
+  .name('gendiff')
+  .description('  Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format (default: "stylish")')
+  .argument('<filepath1>', 'first file path')
+  .argument('<filepath2>', 'second file path')
+  .action((filepath1, filepath2, options) => {
+      const result = genDiff(filepath1, filepath2, options.format);
+      console.log(result);
+  });
+
+program.parse();
---
+++ src/differ/differ.js
@@ -0,0 +1,6 @@
+function genDiff (firstContext, secondContext) {
+  console.log('Первый файл', firstContext);
+  console.log('Второй файл:', secondContext);
+}
+
+export { genDiff };
