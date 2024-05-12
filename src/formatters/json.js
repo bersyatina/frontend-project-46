bc93590a9e89613a057ee6a389e8dde26dc78e6d
bersyatina <aw_apanasenko@mail.ru>
2024-05-12 20:25:49 +0500

    stage 9

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,64 +1,59 @@
 import { isComparisonObject } from '../index.js';
-import json from '../parsers/json.js';
 
-export const getResultToJson = (resultToArray, path = '') => {
+export const getResultToJson = (resultToArray) => {
+  return getJsonData(resultToArray);
+};
+
+const getPrimitiveValue = (value) => {
+  return typeof value === 'string' ? `"${value}"` : value;
+};
+
+const getJsonData = (resultToArray, path = '') => {
   if (typeof resultToArray !== 'object' || resultToArray === null) {
     return resultToArray;
   }
   let string;
   if (typeof resultToArray === 'object' && !Array.isArray(resultToArray)) {
     string = Object.keys(resultToArray).map((currentKey) => {
-      // console.log(`{"${currentKey}":${resultToArray[currentKey]}}`);
-      let value = getResultToJson(resultToArray[currentKey], path);
+      let value = getJsonData(resultToArray[currentKey], path);
       if (typeof resultToArray[currentKey] !== 'object') {
         value = getPrimitiveValue(value);
       }
-
-      return `{"${currentKey}":${value}}`;
+      return `"${currentKey}":${value}`;
     });
-    string = string.join(',');
-    // console.log(string.length);
-    // console.log(string);
+    string = `{${string}}`;
   } else {
     string = resultToArray.map((item) => {
-      if (typeof item === 'object') {
-        if (isComparisonObject(item)) {
-          const newPath = `${path}.${item.key}`.replace(/^\.+/, '');
-          const filter = resultToArray.filter(
-            (value) => value.key === item.key,
-          );
-          let operation = '';
-          let value = getResultToJson(item.value, newPath);
-
-          if (!Array.isArray(item.value) && typeof item.value !== 'object') {
-            value = getPrimitiveValue(value);
-          }
-
-          switch (item.operator) {
-            case '+':
-              operation = filter.length !== 2 ? 'added' : 'replaced';
-              break;
-            case '-':
-              operation = filter.length !== 2 ? 'removed' : 'replaced by';
-              break;
-            case ' ':
-              operation = 'no changed';
-              break;
-            case '':
-              return Object.keys(item).reduce((acc, key) => {
-                return acc + key + ':' + `"${item[key]}"`;
-              }, '');
-          }
-          console.log(value);
-          return `{"path":"${newPath}","operation":"${operation}","value":${value}}`;
+      if (isComparisonObject(item)) {
+        const newPath = `${path}.${item.key}`.replace(/^\.+/, '');
+        const filter = resultToArray.filter((value) => value.key === item.key);
+        let operation = '';
+        let value = getJsonData(item.value, newPath);
+        // console.log(Array.isArray(item.value));
+        if (!Array.isArray(item.value) && typeof item.value !== 'object') {
+          value = getPrimitiveValue(value);
         }
+
+        switch (item.operator) {
+          case '+':
+            operation = filter.length !== 2 ? 'added' : 'replaced';
+            break;
+          case '-':
+            operation = filter.length !== 2 ? 'removed' : 'replaced by';
+            break;
+          case ' ':
+            operation = 'no changed';
+            break;
+          case '':
+            return Object.keys(item).reduce((acc, key) => {
+              return acc + key + ': ' + item[key];
+            }, '');
+        }
+
+        return `{"path":"${newPath}","operation":"${operation}","value":${value}}`;
       }
     }, resultToArray);
   }
-  // console.log(string);
-  return `[${string}]`;
-};
 
-const getPrimitiveValue = (value) => {
-  return typeof value === 'string' ? `"${value}"` : value;
+  return `[${string}]`;
 };
